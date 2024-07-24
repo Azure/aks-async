82d5e82eeb5571c2cb1c1473d0fb9ba38e7ffda6
Martin <mheberling@microsoft.com>
2024-07-24 16:57:55 -0700

    Added retry through the panic handlers.

--- operationsbus/operation_request.go
+++ operationsbus/operation_request.go
@@ -2,15 +2,12 @@ package operationsbus
 
 // All the fields that the operations might need. This struct will be part of every operation.
 type OperationRequest struct {
-	//TODO(mheberling): figure out which general fields we need to add.
 	OperationName string
-
-	APIVersion string
-
-	OperationId string
+	APIVersion    string
+	RetryCount    int
+	OperationId   string
 
 	// HTTP
 	Body       []byte
 	HttpMethod string
-	RetryCount int
 }
--- operationsbus/receiver.go
+++ operationsbus/receiver.go
@@ -15,11 +15,16 @@ import (
 )
 
 func CreateProcessor(serviceBusReceiver sb.ServiceBusReceiver, matcher *Matcher) (*shuttle.Processor, error) {
+	panicOptions := &shuttle.PanicHandlerOptions{
+		OnPanicRecovered: basicPanicRecovery,
+	}
+
 	//TODO(mheberling): Think if we need to change these time variables.
 	lockRenewalInterval := 10 * time.Second
 	lockRenewalOptions := &shuttle.LockRenewalOptions{Interval: &lockRenewalInterval}
+
 	p := shuttle.NewProcessor(serviceBusReceiver.Receiver,
-		shuttle.NewPanicHandler(nil,
+		shuttle.NewPanicHandler(panicOptions,
 			shuttle.NewRenewLockHandler(lockRenewalOptions,
 				myHandler(matcher))),
 		&shuttle.ProcessorOptions{
@@ -59,37 +64,77 @@ func myHandler(matcher *Matcher) shuttle.HandlerFunc {
 		// 3. Init the operation with the information we have.
 		operation.Init(ctx, body)
 
-		// 4. Get the entity.
-		entity, err := operation.EntityFetcher(ctx)
-		if err != nil {
-			logger.Error("Entity was not able to be retrieved: " + err.Error())
-			panic(err)
+		panicOptions := &shuttle.PanicHandlerOptions{
+			OnPanicRecovered: operationPanicRecovery(operation),
 		}
 
-		// 5. Guard against concurrency.
-		ce, err := operation.Guardconcurrency(ctx, *entity)
-		if err != nil {
-			logger.Error("Error calling GuardConcurrency: " + err.Error())
-			logger.Error("Categorized Error calling GuardConcurrency: " + ce.Error())
-
-			// Retry
-			operationRequest := operation.GetOperationRequest(ctx)
-			operationRequest.RetryCount++
-			retryErr := operation.Retry(ctx, *operationRequest)
-			if retryErr != nil {
-				logger.Error("Error retrying: " + retryErr.Error())
-				panic(retryErr)
+		// We add a different panic handler here because we only want to retry the operation if: 1. We are able to unmarshall the message,
+		// 2. The retry limit has not been exceeded, 3. We were able to instantiate the operation. These 3 things will not change by
+		// returning the message to the queue so we don't want to retry.
+		handler := shuttle.NewPanicHandler(panicOptions, shuttle.HandlerFunc(func(ctx context.Context, settler shuttle.MessageSettler, message *azservicebus.ReceivedMessage) {
+			// 4. Get the entity.
+			entity, err := operation.EntityFetcher(ctx)
+			if err != nil {
+				logger.Error("Entity was not able to be retrieved: " + err.Error())
+				panic(err)
 			}
-			panic(err)
-		}
 
-		// 5. Call run on the operation
-		operation.Run(ctx)
+			// 5. Guard against concurrency.
+			ce, err := operation.Guardconcurrency(ctx, *entity)
+			if err != nil {
+				logger.Error("Error calling GuardConcurrency: " + err.Error())
+				logger.Error("Categorized Error calling GuardConcurrency: " + ce.Error())
+				panic(err)
+			}
 
-		// 6. Finish the message
-		err = settler.CompleteMessage(ctx, message, nil)
+			// 6. Call run on the operation
+			result := operation.Run(ctx)
+			if result.Error != nil {
+				logger.Error("Something went wrong running the operation: " + result.Error.Error())
+				panic(result.Error)
+			}
+
+			// 7. Finish the message
+			settleMessage(ctx, settler, message, nil)
+			logger.Info("Operation run successfully!")
+		}))
+
+		handler.Handle(ctx, settler, message)
+	}
+}
+
+func basicPanicRecovery(ctx context.Context, settler shuttle.MessageSettler, message *azservicebus.ReceivedMessage, recovered any) {
+	logger := ctxlogger.GetLogger(ctx)
+	logger.Info("Recovering from panic before getting operation.")
+
+	// Settle message
+	settleMessage(ctx, settler, message, nil)
+}
+
+func operationPanicRecovery(operation APIOperation) func(ctx context.Context, settler shuttle.MessageSettler, message *azservicebus.ReceivedMessage, recovered any) {
+	return func(ctx context.Context, settler shuttle.MessageSettler, message *azservicebus.ReceivedMessage, recovered any) {
+		logger := ctxlogger.GetLogger(ctx)
+		logger.Info("Recovering from panic after getting operation.")
+
+		// Retry the message
+		operationRequest := operation.GetOperationRequest(ctx)
+		operationRequest.RetryCount++
+		err := operation.Retry(ctx, *operationRequest)
 		if err != nil {
-			panic(err)
+			logger.Error("Error retrying: " + err.Error())
 		}
+
+		// Settle message
+		settleMessage(ctx, settler, message, nil)
+	}
+}
+
+func settleMessage(ctx context.Context, settler shuttle.MessageSettler, message *azservicebus.ReceivedMessage, options *azservicebus.CompleteMessageOptions) {
+	logger := ctxlogger.GetLogger(ctx)
+	logger.Info("Settling message.")
+
+	err := settler.CompleteMessage(ctx, message, options)
+	if err != nil {
+		logger.Error("Unable to settle message.")
 	}
 }
