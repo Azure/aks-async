8c4fb5316a62fc0ddd9cf7bac07a179266570610
Martin <mheberling@microsoft.com>
2024-10-14 11:45:32 -0700

    Added mocks for service bus.

--- operationsbus/categorized_error.go
+++ operationsbus/categorized_error.go
@@ -7,13 +7,15 @@ type CategorizedError struct {
 	Message      string
 	InnerMessage string
 	ErrorCode    int
+	Err          error
 }
 
-func NewCategorizedError(message string, innerMessage string, errorCode int) *CategorizedError {
+func NewCategorizedError(message string, innerMessage string, errorCode int, err error) *CategorizedError {
 	return &CategorizedError{
 		Message:      message,
 		InnerMessage: innerMessage,
 		ErrorCode:    errorCode,
+		Err:          err,
 	}
 }
 
