067beb65d5d8b1296e4504cb8e1d78be40bc8614
Martin <mheberling@microsoft.com>
2024-10-17 10:49:50 -0700

    Added description to all the fields in OperationRequest.

--- operationsbus/operation_request.go
+++ operationsbus/operation_request.go
@@ -14,19 +14,19 @@ import (
 
 // All the fields that the operations might need. This struct will be part of every operation.
 type OperationRequest struct {
-	OperationName  string
-	APIVersion     string
-	RetryCount     int
-	OperationId    string
-	EntityId       string
-	EntityType     string
-	ExpirationDate *timestamppb.Timestamp
+	OperationName       string                 // Name of the operation being processed. Used to match the ApiOperation with the right implementation.
+	APIVersion          string                 // Specifies the version of the API the operation is associated with, ensuring compatibility.
+	RetryCount          int                    // Tracks the number of retries of the operation to prevent infinite looping or special logic around retries.
+	OperationId         string                 // A unique identifier for the operation.
+	EntityId            string                 // A unique identifier for the entity (resource) the operation is acting on, used with EntityType to ensure we have selected the right entity.
+	EntityType          string                 // Specified the type of entity the operation is acting on, used with EntityId to ensure we have selected the right Entity.
+	ExpirationTimestamp *timestamppb.Timestamp // Defines when the operation should expire and prevent execution, should it have passed this date.
 
 	// HTTP
-	Body       []byte
-	HttpMethod string
+	Body       []byte // Contains request payload or data needed for the operation in HTTP operations.
+	HttpMethod string // Indicated the GGPT method if the operation requires HTTP-based communication.
 
-	Extension interface{}
+	Extension interface{} // An optional and flexible field to add any data the user may require.
 }
 
 func NewOperationRequest(
@@ -36,22 +36,22 @@ func NewOperationRequest(
 	entityId string,
 	entityType string,
 	retryCount int,
-	expirationDate *timestamppb.Timestamp,
+	expirationTimestamp *timestamppb.Timestamp,
 	body []byte,
 	httpMethod string,
 	extension interface{},
 ) *OperationRequest {
 	return &OperationRequest{
-		OperationName:  operationName,
-		APIVersion:     apiVersion,
-		RetryCount:     retryCount,
-		OperationId:    operationId,
-		EntityId:       entityId,
-		EntityType:     entityType,
-		ExpirationDate: expirationDate,
-		Body:           body,
-		HttpMethod:     httpMethod,
-		Extension:      extension,
+		OperationName:       operationName,
+		APIVersion:          apiVersion,
+		RetryCount:          retryCount,
+		OperationId:         operationId,
+		EntityId:            entityId,
+		EntityType:          entityType,
+		ExpirationTimestamp: expirationTimestamp,
+		Body:                body,
+		HttpMethod:          httpMethod,
+		Extension:           extension,
 	}
 }
 
