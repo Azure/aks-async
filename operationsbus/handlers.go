e4ba4d4ae78a3224faf27a130477ce59c6866fe0
Martin <mheberling@microsoft.com>
2025-04-28 19:00:05 -0700

    Added logger with fields to NewQoSHandler.

--- operationsbus/handlers.go
+++ operationsbus/handlers.go
@@ -110,7 +110,6 @@ func NewQosErrorHandler(errHandler ErrorHandlerFunc) shuttle.HandlerFunc {
 				"start_time", start.String(),
 				"end_time", t.String(),
 				"latency", elapsed.String(),
-				"error", err.Error(),
 			).Info("QoS: Operation processed successfully. No errors returned.")
 		}
 	}
@@ -127,7 +126,11 @@ func NewQosHandler(logger *slog.Logger, next shuttle.HandlerFunc) shuttle.Handle
 		next(ctx, settler, message)
 		t := time.Now()
 		elapsed := t.Sub(start)
-		logger.Info("QoS: Operation started at: " + start.String() + ". QoS: Operation processed at: " + t.String() + ". QoS: Operation took " + elapsed.String() + " to process.")
+		logger.With(
+			"start_time", start.String(),
+			"end_time", t.String(),
+			"latency", elapsed.String(),
+		).Error("QoS: Error ocurrent in next handler.")
 	}
 }
 
